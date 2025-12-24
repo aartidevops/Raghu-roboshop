f389a18c6b825aba845224a5db71a45330ab5b94
Aarti chaple <aartichaple2124@gmail.com>
2025-12-24 20:43:59 +0530

    test

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_security_group" "main" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = ["*"]
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
