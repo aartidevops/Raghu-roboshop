a51daf3fc2103cb9c918fde0748e4218d5f046d5
Aarti chaple <aartichaple2124@gmail.com>
2026-01-17 16:06:02 +0530

    test

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -13,6 +13,11 @@ resource "helm_release" "external-secrets" {
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
   namespace  = "kube-system"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 }
 
 resource "null_resource" "external-secrets" {
