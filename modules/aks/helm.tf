67bc2205a658a701f871a4a069b5491a7b380b6e
Aarti chaple <aartichaple2124@gmail.com>
2026-01-30 13:55:08 +0530

    test

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,32 +1,33 @@
-# resource "null_resource" "kubeconfig" {
-#   depends_on = [azurerm_kubernetes_cluster.main]
-#   provisioner "local-exec" {
-#     command = <<EOF
-# az aks get-credentials --resource-group ${data.azurerm_resource_group.default.name} --name aks --overwrite-existing
-# EOF
-#   }
-# }
-#
-# resource "helm_release" "external-secrets" {
-#   depends_on = [null_resource.kubeconfig]
-#   name       = "external-secrets"
-#   repository = "https://charts.external-secrets.io"
-#   chart      = "external-secrets"
-#   namespace  = "kube-system"
-#
-#   set = [
-#     {
-#       name  = "installCRDs"
-#       value = "true"
-#     }
-#   ]
-# }
-#
-# resource "null_resource" "external-secrets" {
-#   depends_on = [helm_release.external-secrets]
-#   provisioner "local-exec" {
-#     command = <<EOF
-# kubectl apply -f /opt/vault-token.yml
-# EOF
-#   }
-# }
+resource "null_resource" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.main]
+  provisioner "local-exec" {
+    command = <<EOF
+az aks get-credentials --resource-group ${data.azurerm_resource_group.default.name} --name aks --overwrite-existing
+EOF
+  }
+}
+
+resource "helm_release" "external-secrets" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = "kube-system"
+
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
+}
+
+resource "null_resource" "external-secrets" {
+  depends_on = [helm_release.external-secrets]
+  provisioner "local-exec" {
+    command = <<EOF
+kubectl apply -f /opt/vault-token.yml
+kubectl apply -f ${path.module}/files/secretStore.yaml
+EOF
+  }
+}
