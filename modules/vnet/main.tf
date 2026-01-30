ab0356d9d36efbb23cf778b10aea6926d276526f
Aarti chaple <aartichaple2124@gmail.com>
2026-01-30 18:23:29 +0530

    test

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -17,16 +17,16 @@ resource "azurerm_subnet" "main" {
   address_prefixes     = [var.subnets[count.index]]
 }
 
-# resource "azurerm_virtual_network_peering" "main-to-project" {
-#   name                      = "${var.rg_name}-to-project"
-#   resource_group_name       = var.rg_name
-#   virtual_network_name      = azurerm_virtual_network.main.name
-#   remote_virtual_network_id = data.azurerm_virtual_network.project.id
-# }
-#
-# resource "azurerm_virtual_network_peering" "project-to-mai" {
-#   name                      = "project-to-${var.rg_name}"
-#   resource_group_name       = data.azurerm_resource_group.default.name
-#   virtual_network_name      = data.azurerm_virtual_network.project.name
-#   remote_virtual_network_id = azurerm_virtual_network.main.id
-# }
+resource "azurerm_virtual_network_peering" "main-to-project" {
+  name                      = "${var.rg_name}-to-project"
+  resource_group_name       = var.rg_name
+  virtual_network_name      = azurerm_virtual_network.main.name
+  remote_virtual_network_id = data.azurerm_virtual_network.project.id
+}
+
+resource "azurerm_virtual_network_peering" "project-to-main" {
+  name                      = "project-to-${var.rg_name}"
+  resource_group_name       = data.azurerm_resource_group.default.name
+  virtual_network_name      = data.azurerm_virtual_network.project.name
+  remote_virtual_network_id = azurerm_virtual_network.main.id
+}
