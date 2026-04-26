904aa57d63f82a652c9d5b1e1056c024fe9d7eb2
Aarti chaple <aartichaple2124@gmail.com>
2026-04-26 23:22:51 +0530

    test

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-roboshop-tfstate"
-    storage_account_name = "roboshoptfstateXXXX"  # paste from bootstrap.sh output
+    storage_account_name = "roboshoptfstate50188763"  # paste from bootstrap.sh output
     container_name       = "tfstate"
     key                  = "dev/roboshop.tfstate"
   }
