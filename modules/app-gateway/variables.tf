3aad32897538686bef7243987f8f476e0ece4be2
Aarti chaple <aartichaple2124@gmail.com>
2026-04-26 19:17:41 +0530

    test

--- modules/app-gateway/variables.tf
+++ modules/app-gateway/variables.tf
@@ -3,4 +3,7 @@ variable "env"                 { type = string }
 variable "location"            { type = string }
 variable "resource_group_name"{ type = string }
 variable "agw_subnet_id"       { type = string }
-variable "tags"                { type = map(string); default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
