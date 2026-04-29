a1fdeeaabcdb68165a703cf87db029cd3e6c6e12
Aarti chaple <aartichaple2124@gmail.com>
2026-04-29 20:03:13 +0530

    test

--- platform/backend.tf
+++ platform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-roboshop-tfstate"
     storage_account_name = "roboshoptfstate"
     container_name       = "tfstate"
-    key                  = "dev/roboshop.tfstate"
+    key                  = "dev/platform.tfstate"
     # infra uses dev/roboshop.tfstate
     # platform uses dev/platform.tfstate
     # Two separate state files — they never interfere
