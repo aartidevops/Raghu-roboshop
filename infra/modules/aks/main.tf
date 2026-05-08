86509de0d9975cb345fb340c52e8dbc6654736b4
Aarti chaple <aartichaple2124@gmail.com>
2026-05-08 13:15:35 +0530

    test

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -48,9 +48,9 @@ resource "azurerm_kubernetes_cluster_node_pool" "workload" {
   tags = var.tags
 }
 
-# # AKS pulls images from ACR without stored credentials
-# resource "azurerm_role_assignment" "acr_pull" {
-#   scope                = var.acr_id
-#   role_definition_name = "AcrPull"
-#   principal_id = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
-# }
+# AKS pulls images from ACR without stored credentials
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = var.acr_id
+  role_definition_name = "AcrPull"
+  principal_id = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
